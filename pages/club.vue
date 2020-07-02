1cf9879fc88f70794eb5ae19f2f61a8bab154ae1
biboy1999 <biboy1999@gmail.com>
2020-07-03 04:59:54 +0800

    add club pages

--- pages/club.vue
+++ pages/club.vue
@@ -1,17 +1,26 @@
 <template>
   <v-layout column justify-center>
-    <h1>所有社團</h1>
+    <h1>{{clubName}}</h1>
     <v-container fluid>
-      <v-row dense>
-        <v-col v-for="club in clubs" :key="club.id" :cols="6">
+      <v-row align="center" justify="center">
+        <v-col v-for="post in posts" :key="post.title" :cols="8">
           <v-card raised>
+            <v-card-title>
+              <span class="title font-weight-light">{{post.title}}</span>
+            </v-card-title>
             <v-card-text>
-                <p>ouo</p>
+              <p>{{post.context}}</p>
             </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" >前往社團看版</v-btn>
-            </v-card-actions>
+            <v-carousel v-if="post.Imagelist.length !== 0">
+              <v-carousel-item
+                v-for="(img,i) in post.Imagelist"
+                :key="i"
+                :src="img.src"
+                :href="img.src"
+                reverse-transition="fade-transition"
+                transition="fade-transition"
+              ></v-carousel-item>
+            </v-carousel>
           </v-card>
         </v-col>
       </v-row>
@@ -26,12 +35,30 @@ import VuetifyLogo from "~/components/VuetifyLogo.vue";
 export default {
   data() {
     return {
-      clubs: null
+      clubName: null,
+      posts: []
     };
   },
   async mounted() {
-    let resp = await this.$nuxt.$axios.get("/club")
-    this.club = resp.data;
+    const params = new URLSearchParams(window.location.search);
+    var id = params.get("id");
+    let resp = await this.$nuxt.$axios.get("/club/" + id + "/post");
+    console.log(resp.data);
+    this.clubName = resp.data[0].name;
+    resp.data[0].post.forEach(e => {
+      var imglist = [];
+      if (e.imageList.split(",")[0] !== "") {
+        imglist = e.imageList
+          .split(",")
+          .map(x => ({ src: "http://i.imgur.com/" + x }));
+      }
+      this.posts.push({
+        title: e.title,
+        context: e.context,
+        Imagelist: imglist,
+        createAt: e.createAt
+      });
+    });
   }
 };
 </script>
