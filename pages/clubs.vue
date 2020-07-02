b2eb0a5273bf942da9a511478b6bb62d8cbca13e
biboy1999 <biboy1999@gmail.com>
2020-07-03 00:31:43 +0800

    add clubs.vue

--- pages/clubs.vue
+++ pages/clubs.vue
@@ -1,19 +1,39 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
+  <v-layout column justify-center>
+    <h1>所有社團</h1>
+    <v-container fluid>
+      <v-row dense>
+      <v-col v-for="club in clubs" :key="club.id" :cols="6">
+        <v-card raised>
+          <v-card-text>
+            <p class="display-1 text--primary">{{club.name}}</p>
+            <div class="text--primary"  style="white-space: pre-line">{{club.description}}</div>
+            <br>
+            <p class="text--secondary">值班時間：{{club.dutyTime}}</p>
+            <p class="text--secondary">值班地點：{{club.clubLocation}}</p>
+            <p class="text--secondary">社課時間：{{club.meetTime}}</p>
+            <p class="text--secondary">社課地點：{{club.meetLocation}}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      </v-row>
+    </v-container>
   </v-layout>
 </template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  data() {
+    return {
+      clubs: null
+    };
+  },
+  mounted() {
+    this.$nuxt.$axios.get("/club").then(resp => (this.clubs = resp.data));
+    console.log("DOOM");
+  }
+};
+</script>
